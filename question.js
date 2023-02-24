68f92c4847d2e3c1d287c89c0f8176beb1611961
Tanvir Hussain <64609749+tanvirsyl@users.noreply.github.com>
2023-02-24 20:11:11 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,55 @@
+let questions = [
+  {
+    numb: 1,
+    question: "A line which cuts a pair of parallel lines is called?",
+    answer: "traversal",
+    options: ["tangent", "chord", "traversal", "intersector"],
+  },
+  {
+    numb: 2,
+    question: "___, is called complete angle?",
+    answer: "240°",
+    options: ["180°", "240°", "360°", "none of the above"],
+  },
+  {
+    numb: 3,
+    question: "What will come, 5, 7, 12, 19, 31, 50, ?",
+    answer: "81",
+    options: ["81", "74", "69", "none of the above"],
+  },
+
+  {
+    numb: 4,
+    question: "find the value of (175)2 – (75)2 =?",
+    answer: "25000",
+    options: ["5000", "20000", "25000", "None of the above"],
+  },
+
+  {
+    numb: 5,
+    question: "If a+b:b+c:c+a=6:7:8 and a+b+c=14,then c=",
+    answer: "(6",
+    options: ["6", "7", "8", "14"],
+  },
+
+  {
+    numb: 6,
+    question: "The average of the first five multiples of 3 is?",
+    answer: "9",
+    options: ["3", "9", "12", "15"],
+  },
+
+  {
+    numb: 7,
+    question: "If Px=Qy=Rz and Q/P=R/Q,then 2z/(x+z)=",
+    answer: "y/x",
+    options: ["y/z", "y/x", "x/y", "z/y"],
+  },
+
+  {
+    numb: 8,
+    question: "4950/6 + 112 x 1.75 = ? x 2?",
+    answer: "510.5",
+    options: ["510.5", "505.2", "515.5", "none of the above"],
+  },
+]
